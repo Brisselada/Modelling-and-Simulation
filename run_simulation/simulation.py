2004cc36bf29603d9a1820b71f0bf0cb5ff6dc29
JesperKarsten <jespert.karsten@gmail.com>
2021-10-24 16:56:50 +0200

    Added manual tl program

--- run_simulation/simulation.py
+++ run_simulation/simulation.py
@@ -4,16 +4,18 @@ import traci.constants as tc
 from collections.abc import Callable
 import numpy as np
 from dataclasses import dataclass
+from typing import Union
+
 
 class simulation:
-    def __init__(self,grid_path: str ="../generate_network/grid.sumocfg") -> None:
+    def __init__(self, grid_path: str = "../generate_network/grid.sumocfg") -> None:
         self.sumoCmd = ["sumo", "-c", grid_path]
 
     def prep_data(self) -> None:
         # Getting all traffic light lane connections
         self.all_tl = traci.trafficlight.getIDList()
 
-        # Final data sctructures contain lanes reading to tl and from tl
+        # Final data structures contain lanes reading to tl and from tl
         self.tl_to_lanes = {}
         self.tl_from_lanes = {}
 
@@ -25,10 +27,10 @@ class simulation:
         lanes = traci.lane.getIDList()
 
         for l in lanes:
-            # this might be due to a bug in traci, but it it gets all lanes twice, the first time in a weird format starting with ":", so we skip those
+            # this might be due to a bug in traci, but it it gets all lanes twice, the first time in a weird format
+            # starting with ":", so we skip those
             if l[0]==":":
                 continue
-            print(l)
             lane_from_edge = l[0:2]
             lane_to_edge = l[2:4]
 
@@ -36,10 +38,6 @@ class simulation:
                 self.tl_from_lanes[lane_from_edge].append(l)
             if lane_to_edge in self.tl_to_lanes:
                 self.tl_to_lanes[lane_to_edge].append(l)
-
-        print(self.tl_from_lanes)
-        print("")
-        print(self.tl_to_lanes)
     def start_sim(self,timestep: int = 1,endstep : int = 10000):
         mean_speeds = []
         mean_times = []
@@ -57,9 +55,9 @@ class simulation:
             for ID in all_vehicles:
                 speed_step.append(traci.vehicle.getSpeed(ID))
                 time_step.append(traci.vehicle.getAccumulatedWaitingTime(ID))
-            if len(speed_step)>0:
+            if len(speed_step) > 0:
                 mean_speeds.append(sum(speed_step)/len(speed_step))
-            if len(time_step)>0:
+            if len(time_step) > 0:
                 mean_times.append(sum(time_step)/len(time_step))
             traci.simulationStep()
             step += timestep
