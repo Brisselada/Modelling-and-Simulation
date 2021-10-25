33d06c870daa118dafcefd93acf9358049893cc7
Bauke Risselada <brisselada@live.nl>
2021-10-25 11:58:24 +0200

    Add queue sized and global strategies

--- run_simulation/simulation.py
+++ run_simulation/simulation.py
@@ -61,9 +61,82 @@ class simulation:
                 mean_speeds.append(sum(speed_step)/len(speed_step))
             if len(time_step)>0:
                 mean_times.append(sum(time_step)/len(time_step))
+            # self.eval_tls_queuesize()
+            # self.eval_tls_global()
             traci.simulationStep()
             step += timestep
 
         traci.close()
 
         return mean_speeds,mean_times
+
+    # Evaluates all traffic lights in the system (alleen op 4-way intersections?) and sets the new corresponding state
+    # Uses the queue size based strategy
+    def eval_tls_queuesize(self):
+        for junc in traci.junction.getIDList(): # Alleen de middelste junctions beschouwen?
+            # Kan geen duidelijke data structure vinden voor combinations,
+            # waarschijnlijk ook custom data structure voor nodig? En moet mogelijk in setup
+            # opgebouwd worden?
+            for TL_combination in junc.combinations:
+                TL_combination.score = 0
+                for TL in TL_combination:
+                    lane = traci.trafficlight.getControlledLanes(TL)[0] # Each TL should control 1 lane
+                    TL_combination.score += self.getNumVehicles(lane)
+            newPhase = self.getNewCombination(junc.combinations) # Moet nog combination aan phase koppelen
+            # Nog beslissen wat beste manier van phase setten is, enkele manieren beschikbaar
+            junc.setPhase(newPhase)
+
+    # Evaluates all traffic lights in the system (alleen op 4-way intersections?) and sets the new corresponding state
+    # Uses the smart/global strategy
+    # Overwegend hetzelfde als queue size based strategy, maar dan met extra phase en de extra connectedScore
+    def eval_tls_global(self):
+        # Phase 1: First calculate all connected scores for each TL
+        for TL in traci.trafficlight.getIDList(): # Alleen de stoplichten van middelste junctions beschouwen?
+            for connectedEdge in TL.connectedEdges: # only 1 in our case, since each lane has a distinct direction
+                for CE_TL in connectedEdge.trafficLights: # iterate TL's corresponding to connected edge
+                    # Hebben wss aparte data structure nodig om connected scores op te slaan
+                    CE_TL.connectedScore += (1 / len(connectedEdge.trafficLights)) # usually 1/3
+
+        # Phase 2: Add connected score and own score, determine new TL combination
+        connectedFactor = 0.3 # Nog definen als constant
+        for junc in traci.junction.getIDList(): # Alleen de middelste junctions beschouwen?
+            # Kan geen duidelijke data structure vinden voor combinations,
+            # waarschijnlijk ook custom data structure voor nodig? En moet mogelijk in setup
+            # opgebouwd worden?
+            for TL_combination in junc.combinations:
+                TL_combination.score = 0
+                for TL in TL_combination:
+                    lane = traci.trafficlight.getControlledLanes(TL)[0] # Each TL should control 1 lane
+                    ownScore = self.getNumVehicles(lane)
+                    TL_combination.score += ownScore + (TL.ConnectedScore * connectedFactor)
+            newPhase = self.getNewCombination(junc.combinations) # Moet nog combination aan phase koppelen
+            # Nog beslissen wat beste manier van phase setten is, enkele manieren beschikbaar
+            junc.setPhase(newPhase)
+
+
+    # Returns number of vehicles on the given lane, within X distance of the junction
+    def getNumVehicles(self, lane):
+        total = 0
+        for vehicle in traci.lane.getLastStepVehicleIDs(lane):
+            if traci.vehicle.getLanePosition(vehicle) < 90: # Moet wss specifieker
+                total += 1
+        return total
+
+    # Returns the traffic light combination with the highest score
+    def getNewCombination(self, combinations):
+        highScore = 0
+        newCombination = [] # The new traffic light combination
+        for combination in combinations:
+            if combination.score > highScore:
+                combination.score = highScore
+                newCombination = combination
+        return newCombination
+
+
+
+
+
+
+
+
+
