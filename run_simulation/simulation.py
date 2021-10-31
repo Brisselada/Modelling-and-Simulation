4418f16b403d0cc1ce4b60c065bafb882ae3eda1
Bauke Risselada <brisselada@live.nl>
2021-10-31 09:41:01 +0100

    Add dict, updated TL strategies

--- run_simulation/simulation.py
+++ run_simulation/simulation.py
@@ -1,4 +1,6 @@
 import os, sys
+from collections import defaultdict
+
 import traci
 import traci.constants as tc
 from collections.abc import Callable
@@ -31,6 +33,7 @@ class Combination:
     def __init__(self, ryg_state: str,corresponding_lanes: list) -> None:
         self.ryg_state = ryg_state
         self.corresponding_lanes = corresponding_lanes
+        self.score = 0
 
 
 class simulation:
@@ -48,6 +51,9 @@ class simulation:
         letters = string.ascii_uppercase
         excluded = [letters[0] + "0", letters[0] + str(self.gridsize-1), letters[self.gridsize-1] + "0",
                     letters[self.gridsize-1] + str(self.gridsize-1)]
+
+        self.lane_scores = defaultdict(int)
+        self.connectedFactor = 0.3
 
         self.junctions = []
         for ID in traffic_lights:
@@ -119,46 +125,44 @@ class simulation:
     # Evaluates all traffic lights in the system (alleen op 4-way intersections?) and sets the new corresponding state
     # Uses the queue size based strategy
     def eval_tls_queuesize(self):
-        for junc in traci.junction.getIDList(): # Alleen de middelste junctions beschouwen?
-            # Kan geen duidelijke data structure vinden voor combinations,
-            # waarschijnlijk ook custom data structure voor nodig? En moet mogelijk in setup
-            # opgebouwd worden?
-            for TL_combination in junc.combinations:
-                TL_combination.score = 0
-                for TL in TL_combination:
-                    lane = traci.trafficlight.getControlledLanes(TL)[0] # Each TL should control 1 lane
-                    TL_combination.score += self.getNumVehicles(lane)
-            newPhase = self.getNewCombination(junc.combinations) # Moet nog combination aan phase koppelen
+        for junc in self.junctions:
+            for tl_combination in junc.tl_combinations:
+                tl_combination.score = 0
+                for lane in tl_combination.corresponding_lanes:
+                    tl_combination.score += self.getNumVehicles(lane.ID)
+            newState = self.getNewRYGState(junc.tl_combinations)
             # Nog beslissen wat beste manier van phase setten is, enkele manieren beschikbaar
-            junc.setPhase(newPhase)
+            traci.trafficlight.setRedYellowGreenState(junc, newState)
+
 
     # Evaluates all traffic lights in the system (alleen op 4-way intersections?) and sets the new corresponding state
     # Uses the smart/global strategy
     # Overwegend hetzelfde als queue size based strategy, maar dan met extra phase en de extra connectedScore
     def eval_tls_global(self):
-        # Phase 1: First calculate all connected scores for each TL
-        for TL in traci.trafficlight.getIDList(): # Alleen de stoplichten van middelste junctions beschouwen?
-            for connectedEdge in TL.connectedEdges: # only 1 in our case, since each lane has a distinct direction
-                for CE_TL in connectedEdge.trafficLights: # iterate TL's corresponding to connected edge
-                    # Hebben wss aparte data structure nodig om connected scores op te slaan
-                    CE_TL.connectedScore += (1 / len(connectedEdge.trafficLights)) # usually 1/3
+        # # Phase 1: First calculate all connected scores for each TL
+        # for TL in traci.trafficlight.getIDList(): # Alleen de stoplichten van middelste junctions beschouwen?
+        #     for connectedEdge in TL.connectedEdges: # only 1 in our case, since each lane has a distinct direction
+        #         for CE_TL in connectedEdge.trafficLights: # iterate TL's corresponding to connected edge
+        #             # Hebben wss aparte data structure nodig om connected scores op te slaan
+        #             CE_TL.connectedScore += (1 / len(connectedEdge.trafficLights)) # usually 1/3
+
+        # Phase 1: Calculate own score for each TL
+        for junc in self.junctions:
+            for lane in junc.connected_lanes:  # Each TL should control 1 lane
+                self.lane_scores[lane.ID] = self.getNumVehicles(lane.ID)
 
         # Phase 2: Add connected score and own score, determine new TL combination
-        connectedFactor = 0.3 # Nog definen als constant
-        for junc in traci.junction.getIDList(): # Alleen de middelste junctions beschouwen?
-            # Kan geen duidelijke data structure vinden voor combinations,
-            # waarschijnlijk ook custom data structure voor nodig? En moet mogelijk in setup
-            # opgebouwd worden?
-            for TL_combination in junc.combinations:
-                TL_combination.score = 0
-                for TL in TL_combination:
-                    lane = traci.trafficlight.getControlledLanes(TL)[0] # Each TL should control 1 lane
-                    ownScore = self.getNumVehicles(lane)
-                    TL_combination.score += ownScore + (TL.ConnectedScore * connectedFactor)
-            newPhase = self.getNewCombination(junc.combinations) # Moet nog combination aan phase koppelen
+        for junc in self.junctions:
+            for tl_combination in junc.tl_combinations:
+                tl_combination.score = 0
+                for lane in tl_combination.corresponding_lanes:
+                    connected_score = 0
+                    for connected_lane in lane.previous_tl_connected_lanes:
+                        connected_score += self.lane_scores[connected_lane.ID]
+                    tl_combination.score += self.lane_scores[lane.ID] + (connected_score * self.connectedFactor)
+            newState = self.getNewRYGState(junc.tl_combinations)
             # Nog beslissen wat beste manier van phase setten is, enkele manieren beschikbaar
-            junc.setPhase(newPhase)
-
+            traci.trafficlight.setRedYellowGreenState(junc, newState)
 
     # Returns number of vehicles on the given lane, within X distance of the junction
     def getNumVehicles(self, lane):
@@ -169,14 +173,14 @@ class simulation:
         return total
 
     # Returns the traffic light combination with the highest score
-    def getNewCombination(self, combinations):
+    def getNewRYGState(self, tl_combinations):
         highScore = 0
-        newCombination = [] # The new traffic light combination
-        for combination in combinations:
+        new_ryg_state = [] # The new traffic light combination
+        for combination in tl_combinations:
             if combination.score > highScore:
                 combination.score = highScore
-                newCombination = combination
-        return newCombination
+                new_ryg_state = combination.ryg_state
+        return new_ryg_state
 
 
 
