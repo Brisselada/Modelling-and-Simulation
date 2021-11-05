fe0084e868e158de4d65ac5aadca4d42d4418cf8
jespertjee <jespert.karsten@gmail.com>
2021-11-05 15:02:32 +0100

    Fixed small merge conflict

--- run_simulation/start.py
+++ run_simulation/start.py
@@ -4,11 +4,7 @@ from simulation import simulation
 
 if __name__ == '__main__':
     timestep = 1
-<<<<<<< HEAD
     endstep = 5000
-=======
-    endstep = 10000
->>>>>>> ec68e1f7fc3c0dfbfe745f296ff956a828f933e0
 
     s = simulation(5)
     mean_speeds, mean_times = s.start_sim(timestep,endstep)
